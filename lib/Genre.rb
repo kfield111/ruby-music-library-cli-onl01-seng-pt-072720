aef3170150e11c70302541c2a14d6213721b801e
Kelin Field <kfield111@hotmail.com>
2020-09-07 06:03:28 +0000

    Automatically backed up by Learn

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -2,12 +2,13 @@ class Genre
   attr_accessor :name
   attr_reader :songs
 
+    extend Concerns::Findable
+
   @@all = []
 
   def initialize (name)
     @name = name
     @songs = []
-    self.save
   end
 
   def save
@@ -22,13 +23,23 @@ class Genre
     @@all.clear
   end
 
-  def self.create (name)
-    self.new (name)
-    self
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
   end
 
   def songs
     @songs
+  end
+
+  def add_song(song)
+    song.genre = self if song.genre == nil
+    @songs << song unless @songs.include?(song)
+  end
+
+  def artists
+    self.songs.collect {|temp| temp.artist}.uniq
   end
 
 

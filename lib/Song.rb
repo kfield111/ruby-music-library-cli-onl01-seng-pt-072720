7b18f9baebbf40ece1fd6276fd3b0b64e20dbce4
Kelin Field <kfield111@hotmail.com>
2020-09-07 06:03:50 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -33,10 +33,46 @@ class Song
     @@all.clear
   end
 
-  def self.create (name)
-    song = self.new (name)
+  def self.create(name)
+    song = Song.new(name)
     song.save
     song
+  end
+
+
+  def self.find_by_name(name)
+    self.all.detect {|temp| temp.name == name}
+  end
+
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+
+  def self.new_from_filename(filename)
+    raw_data = filename.split(".mp3").shift
+    data = raw_data.split(" - ")
+    artist = data[0]
+    name = data[1]
+    genre = data[2]
+
+    new_song = self.new(name)
+    artist = Artist.find_or_create_by_name(artist)
+    genre = Genre.find_or_create_by_name(genre)
+
+    new_song.artist = artist
+    new_song.genre = genre
+    new_song
+  end
+
+  def self.create_from_filename(filename)
+    new_song = self.new_from_filename(filename)
+    @@all << new_song
   end
 
 
